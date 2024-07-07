b31f748d7769e8a7f6adaa24f1b5a02394f344f3
hansemin <han299792@gmail.com>
2024-07-07 15:11:38 +0900

    데이터 파일 생성

---
+++ src/mockData.ts
@@ -0,0 +1,15 @@
+const data = 
+[
+{
+   "userId": 1,
+   "id": 1,
+   "title": "test title one",
+   "body": "test body one"
+ },
+  {
+   "userId": 2,
+   "id": 2,
+   "title": "test title two",
+   "body": "test body two"
+ }
+ ]
